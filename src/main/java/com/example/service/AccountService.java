4c349e2ab3f4c945b73bb663f9e77e69b46ab18e
Osvaldo  Mesa <d3v.ozzy@gmail.com>
2024-10-07 22:40:31 +0000

    pass User Registration Test

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,32 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.exception.AccountAlreadyExistsException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account addUser(Account account) {
+        Optional<Account> existingAccount = accountRepository.findByUsername(account.getUsername());
+        if(existingAccount.isPresent()) {
+            throw new AccountAlreadyExistsException("Account already in the database");
+        }
+        if (account.getUsername().length() != 0 && account.getPassword().length() > 3) {
+            return accountRepository.save(account);
+        }
+        return null;
+    }
 }
